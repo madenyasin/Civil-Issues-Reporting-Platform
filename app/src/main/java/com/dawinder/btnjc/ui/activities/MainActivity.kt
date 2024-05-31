013b6a2ae810804548c652bb739f17ec4e9410a3
Yasin Maden <maden.yasin@hotmail.com>
2024-05-31 21:07:04 +0300

    kullanıcı adı profilde gözüküyor

--- app/src/main/java/com/dawinder/btnjc/ui/activities/MainActivity.kt
+++ app/src/main/java/com/dawinder/btnjc/ui/activities/MainActivity.kt
@@ -6,11 +6,16 @@ import android.os.Bundle
 import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.viewModels
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.compose.rememberNavController
 import com.dawinder.btnjc.R
 import com.dawinder.btnjc.ui.composables.MainScreen
@@ -21,9 +26,12 @@ import com.google.android.gms.auth.api.identity.Identity
 import com.google.android.gms.auth.api.identity.SignInClient
 import com.google.android.gms.common.api.ApiException
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
 import com.google.firebase.auth.GoogleAuthProvider
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.ktx.Firebase
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
 
 const val USER = "user-info"
 
@@ -40,6 +48,10 @@ class MainActivity : ComponentActivity() {
     private var showOneTapUI = true
     private lateinit var oneTapClient: SignInClient
     private lateinit var signInRequest: BeginSignInRequest
+
+    private val viewModel by viewModels<AuthViewModel> {
+        AuthViewModelFactory(Firebase.auth)
+    }
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -63,15 +75,12 @@ class MainActivity : ComponentActivity() {
         setContent {
             BottomTabNavigationJetpackComposeTheme {
                 val navController = rememberNavController()
+                val userData by viewModel.userData.collectAsState()
                 Surface(modifier = Modifier.fillMaxSize()) {
                     MainScreen(
                         navController = navController,
                         { signIn() },
-                        UserData(
-                            userId = Firebase.auth.currentUser?.uid ?: "Unknown",
-                            username = Firebase.auth.currentUser?.displayName,
-                            profilePictureUrl = Firebase.auth.currentUser?.photoUrl.toString()
-                        )
+                        userData
                     )
                 }
             }
@@ -120,13 +129,14 @@ class MainActivity : ComponentActivity() {
                                         Log.d(TAG, "signInWithCredential:success")
                                         val user = auth.currentUser
                                         //updateUI(user)
+                                        viewModel.updateUser(auth.currentUser)
                                         Log.d(USER, "signInWithCredential:success")
                                         Log.d(USER, "${user?.displayName}")
                                     } else {
                                         // If sign in fails, display a message to the user
                                         Log.w(TAG, "signInWithCredential:failure", task.exception)
                                         //updateUI(null)
-
+                                        viewModel.updateUser(null)
                                         Log.d(USER, "signInWithCredential:failure")
                                     }
                                 }
@@ -148,8 +158,8 @@ class MainActivity : ComponentActivity() {
     override fun onStart() {
         super.onStart()
         // Check if user is signed in and update UI accordingly
-        val currentUser = auth.currentUser
-        Log.d(USER, "Current User: $currentUser")
+        viewModel.updateUser(auth.currentUser)
+        Log.d(USER, "Current User: $auth.currentUser")
     }
 
     private fun getCurrentUserName(firebaseAuth: FirebaseAuth): String {
@@ -167,5 +177,35 @@ fun GreetingPreview() {
         Surface(modifier = Modifier.fillMaxSize()) {
             //MainScreen(navController = navController)
         }
+    }
+}
+
+class AuthViewModel(private val auth: FirebaseAuth) : ViewModel() {
+    private val _userData = MutableStateFlow(UserData("Unknown", "Unknown", null))
+    val userData: StateFlow<UserData> = _userData
+
+    init {
+        auth.addAuthStateListener { firebaseAuth ->
+            val user = firebaseAuth.currentUser
+            updateUser(user)
+        }
+    }
+
+    fun updateUser(user: FirebaseUser?) {
+        val userId = user?.uid ?: "Unknown"
+        val username = user?.displayName ?: "Unknown"
+        val profilePictureUrl = user?.photoUrl?.toString()
+
+        _userData.value = UserData(userId, username, profilePictureUrl)
+    }
+}
+
+class AuthViewModelFactory(private val auth: FirebaseAuth) : ViewModelProvider.Factory {
+    override fun <T : ViewModel> create(modelClass: Class<T>): T {
+        if (modelClass.isAssignableFrom(AuthViewModel::class.java)) {
+            @Suppress("UNCHECKED_CAST")
+            return AuthViewModel(auth) as T
+        }
+        throw IllegalArgumentException("Unknown ViewModel class")
     }
 }
--- app/src/main/java/com/dawinder/btnjc/ui/composables/tabs/ProfileScreen.kt
+++ app/src/main/java/com/dawinder/btnjc/ui/composables/tabs/ProfileScreen.kt
@@ -1,9 +1,11 @@
 package com.dawinder.btnjc.ui.composables.tabs
 
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.sp
@@ -14,14 +16,11 @@ import com.dawinder.btnjc.ui.data.UserData
  */
 @Composable
 fun ProfileScreen(userData: UserData) {
-    Column(horizontalAlignment = Alignment.CenterHorizontally) {
+    Column(modifier = Modifier.fillMaxSize(), horizontalAlignment = Alignment.CenterHorizontally) {
         Text(
-            text = userData.username.toString(),
+            text = userData.username ?: "Unknown",
             textAlign = TextAlign.Center,
-            fontSize = 36.sp,
-            fontWeight = FontWeight.SemiBold
+            fontSize = 36.sp
         )
-
     }
-
 }
