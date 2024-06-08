a1aed652a6a4578218e2a096c3f4ead43ffd6453
Yasin Maden <maden.yasin@hotmail.com>
2024-06-08 17:40:12 +0300

    user picture eklendi (list)

--- app/src/main/java/com/dawinder/btnjc/ui/composables/tabs/ListScreen.kt
+++ app/src/main/java/com/dawinder/btnjc/ui/composables/tabs/ListScreen.kt
@@ -2,13 +2,8 @@ package com.dawinder.btnjc.ui.composables.tabs
 
 import android.util.Log
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Card
@@ -19,18 +14,21 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.unit.dp
-import coil.compose.rememberImagePainter
+import coil.compose.rememberAsyncImagePainter
 import com.dawinder.btnjc.ui.theme.md_theme_light_inversePrimary
 import com.dawinder.btnjc.ui.theme.md_theme_light_onSurface
 import com.dawinder.btnjc.ui.theme.md_theme_light_primary
 import com.dawinder.btnjc.ui.theme.md_theme_light_secondary
 import com.dawinder.btnjc.ui.theme.typography
-import com.google.firebase.Firebase
 import com.google.firebase.database.DataSnapshot
 import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.ValueEventListener
-import com.google.firebase.database.database
+import com.google.firebase.database.ktx.database
+import com.google.firebase.ktx.Firebase
+
+
 
 @Composable
 fun ListScreen() {
@@ -59,10 +57,12 @@ fun ListScreen() {
         fetchPosts()
     }
 
-    Box(modifier = Modifier.fillMaxSize().padding(bottom = 75.dp), contentAlignment = Alignment.Center) {
+    Box(modifier = Modifier
+        .fillMaxSize()
+        .padding(bottom = 75.dp), contentAlignment = Alignment.Center) {
         if (postsState.value.isEmpty()) {
             Text(
-                text = "no_posts_available",
+                text = "No posts available",
                 style = typography.titleLarge,
                 color = md_theme_light_inversePrimary
             )
@@ -87,6 +87,24 @@ fun PostCard(post: Post) {
             modifier = Modifier
                 .padding(16.dp)
         ) {
+            Row(verticalAlignment = Alignment.CenterVertically) {
+                post.userProfilePictureUrl?.let {
+                    Image(
+                        painter = rememberAsyncImagePainter(model = it),
+                        contentDescription = "User Profile Picture",
+                        modifier = Modifier
+                            .size(40.dp)
+                            .clip(CircleShape)
+                    )
+                    Spacer(modifier = Modifier.width(8.dp))
+                }
+                Text(
+                    text = "Posted by: ${post.userName}",
+                    style = typography.bodySmall,
+                    color = md_theme_light_secondary
+                )
+            }
+            Spacer(modifier = Modifier.height(8.dp))
             Text(
                 text = post.title,
                 style = typography.titleMedium,
@@ -97,12 +115,6 @@ fun PostCard(post: Post) {
                 text = post.description,
                 style = typography.bodyLarge,
                 color = md_theme_light_onSurface
-            )
-            Spacer(modifier = Modifier.height(8.dp))
-            Text(
-                text = "Posted by: ${post.userName}",
-                style = typography.bodySmall,
-                color = md_theme_light_secondary
             )
             Spacer(modifier = Modifier.height(8.dp))
             post.latitude?.let { lat ->
@@ -117,7 +129,7 @@ fun PostCard(post: Post) {
             post.imageUrl?.let { url ->
                 Spacer(modifier = Modifier.height(8.dp))
                 Image(
-                    painter = rememberImagePainter(data = url),
+                    painter = rememberAsyncImagePainter(model = url),
                     contentDescription = "Post Image",
                     modifier = Modifier
                         .fillMaxWidth()
@@ -127,4 +139,3 @@ fun PostCard(post: Post) {
         }
     }
 }
-
