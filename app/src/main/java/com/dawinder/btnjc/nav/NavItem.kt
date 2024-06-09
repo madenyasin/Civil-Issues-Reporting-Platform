9c37551698fcb3b64b21fd8f34c6ba395f05d639
Yasin Maden <maden.yasin@hotmail.com>
2024-06-09 18:06:27 +0300

    icon changed

--- app/src/main/java/com/dawinder/btnjc/nav/NavItem.kt
+++ app/src/main/java/com/dawinder/btnjc/nav/NavItem.kt
@@ -4,17 +4,22 @@ import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.List
 import androidx.compose.material.icons.filled.AccountBox
 import androidx.compose.material.icons.filled.Home
+import androidx.compose.material.icons.filled.List
+import androidx.compose.material.icons.filled.LocationOn
 import androidx.compose.material.icons.filled.Person
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.res.painterResource
+import com.dawinder.btnjc.R
 
 sealed class NavItem {
     object Map :
-        Item(path = NavPath.MAP.toString(), title = NavTitle.MAP, icon = Icons.Default.Home)
+        Item(path = NavPath.MAP.toString(), title = NavTitle.MAP, icon = Icons.Default.LocationOn)
 
     object List :
         Item(
             path = NavPath.LIST.toString(),
             title = NavTitle.LIST,
-            icon = Icons.AutoMirrored.Filled.List
+            icon = Icons.Default.List
         )
 
     object Profile :
